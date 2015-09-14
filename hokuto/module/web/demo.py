4d08884ef2fb59385328cec6967ca2bba024e402
Xavier Roger-Machart <xrm@mm.st>
2015-09-14 17:28:34 +0200

    Finalized the demo user creation

--- hokuto/module/web/demo.py
+++ hokuto/module/web/demo.py
@@ -20,11 +20,13 @@
 """ Utilities related to the demo mode """
 
 from datetime import datetime
+from random import random, choice
 from time import time
 
 from flask import render_template, redirect, url_for
+from sqlalchemy.sql import bindparam
 
-from . import app
+from . import app, db
 
 # Accounts will be deleted if they were created more than this amount of seconds ago
 _max_lifetime = 7200 #60*60*2
@@ -32,11 +34,14 @@ _max_lifetime = 7200 #60*60*2
 # Accounts will be deleted if they were not active since this amount of seconds
 _activity_timeout = 1800 #60*30
 
+_reference_user_id = 1
+_demo_shinken_user = 'drgkill'
+
 @app.route('/demo-landing')
 def demo_landing():
     """ This page tells the user that he tried to use a feature that is not available in demo mode """
     return render_template('demo-landing.html')
-
+    
 def is_in_demo():
     """ Returns True if the demo mode is enabled, False otherwise """
     return app.config.get('DEMO', False) == True
@@ -67,8 +72,64 @@ def flush_old_demo_data():
             app.logger.debug('User {0} "{1}" marked for flushing (too old)'.format(u.id, u.username))
             remove = True
         # Check last activity date
-        elif u.last_activity_date < last_activity_limit:
+        elif u.last_activity_date is None or u.last_activity_date < last_activity_limit:
             app.logger.debug('User {0} "{1}" marked for flushing (activity timeout)'.format(u.id, u.username))
             remove = True
         if remove:
             remove_user(u.id)
+
+_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+def create_demo_user():
+    from user import User, remove_user
+    global _characters, _demo_shinken_user
+    #generate a random name and password
+    suffix_size = int(random() * 4 + 4)
+    name = 'user_' + ''.join([choice(_characters) for i in range(suffix_size)])
+    password = ''.join([choice(_characters) for i in range(8)])
+    # Make sure this username actually does not exist
+    while User.query.filter_by(username = name).first():
+        name = 'user_' + ''.join([choice(_characters) for i in range(suffix_size)])
+        
+    # Create a new user
+    user = User(name, password, True)
+    user.shinken_contact = _demo_shinken_user
+    db.session.add(user)
+    db.session.commit()
+    create_default_graph(user.id)
+    create_default_dashboard(user.id)
+    return name, password
+
+def create_default_graph(userid):
+    from grapher import graphTokenTable
+    global _reference_user_id
+    source = graphTokenTable.select()\
+                            .where(graphTokenTable.c.user_id == _reference_user_id)
+                            
+    insertQuery = graphTokenTable.insert()
+    inserts = [{'user_id': userid,
+                'graph_id': row['graph_id'],
+                'key': row['key'],
+                'value': row['value']} for row in db.engine.execute(source)]
+    db.engine.execute(graphTokenTable.insert(), inserts)
+    
+def create_default_dashboard(userid):
+    from dashboard import partsTable, partsConfTable
+    global _reference_user_id
+    configQuery = partsConfTable.select().where(partsConfTable.c.parts_id == bindparam('part_id'))
+    pSource = partsTable.select().where(partsTable.c.user_id == _reference_user_id)
+    pInserts = [dict(row.items()) for row in db.engine.execute(pSource)]
+    pcInserts = []
+    for i in pInserts:
+        if 'id' in i:
+            i['user_id'] = userid
+            oldid = i['id']
+            del i['id']
+            newid = db.engine.execute(partsTable.insert(), [i]).lastrowid
+            confdata = [dict(row.items()) for row in db.engine.execute(configQuery, part_id=oldid)]
+            for data in confdata:
+                data['parts_id'] = newid
+            pcInserts = pcInserts + confdata
+    db.engine.execute(partsConfTable.insert(), pcInserts)
+    #parts (user_id)
+    #parts_conf (parts_id)
+    pass
