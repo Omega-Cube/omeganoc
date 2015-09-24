793253b70b8a14bb5b8778945d9b28c9368246c6
Guillaume Seigneuret <gs@omegacube.fr>
2015-09-24 16:27:07 +0200

    Vagrant creation improved - SNMP now works, fix some missing python packages

--- vagrant_provision/bootstrap.sh
+++ vagrant_provision/bootstrap.sh
@@ -3,12 +3,24 @@
 export DEBIAN_FRONTEND=noninteractive
 export PERL_MM_USE_DEFAULT=true
 
+#Configure the source.list
+cp /vagrant/vagrant_provision/source.list.template /etc/apt/sources.list 
+
 aptitude -q update
 aptitude -y -q full-upgrade
 aptitude -y -q install vim git python-pip python-pycurl sqlite3 graphviz graphviz-dev pkg-config python-dev libxml2-dev
+aptitude -y -q install snmpd snmp snmp-mibs-downloader
+apt-get  -y -o APT::Install-Recommends=0 install nagios-nrpe-plugin nagios-plugins nagios-plugins-basic nagios-plugins-standard nagios-plugins-contrib nagios-snmp-plugins
+aptitude -y -q install openntpd
+chmod u+s /usr/lib/nagios/plugins/check_icmp
+#download-mibs
+
+# Configure ntp
+cp /vagrant/vagrant_provision/ntpd.conf.template /etc/openntpd/ntpd.conf
 
 # Configure snmpd
 cp /vagrant/vagrant_provision/snmpd.conf.template /etc/snmp/snmpd.conf
+sed -i 's/mibs/#mibs/' /etc/snmp/snmp.conf
 service snmpd restart
 
 # Configure PERL for the SNMP probes
@@ -16,7 +28,7 @@ cpan install Net::SNMP
 
 useradd --user-group shinken
 useradd --user-group graphite
-pip install pycurl cherrypy
+pip install pycurl cherrypy nagiosplugin noise
 cd /vagrant
 git submodule init
 git submodule update
@@ -26,13 +38,14 @@ make shinken-install
 shinken --init
 shinken install linux-snmp
 
+
 # Fix a missing file in linux-snmp
 cp /vagrant/vagrant_provision/utils.pm.template /var/lib/shinken/libexec/utils.pm
 
 # Configure Shinken
-cp /vagrant/vagrant_provision/broker-master.cfg.template /etc/shinken/brokers/broker-master.cfg
-cp /vagrant/vagrant_provision/localhost.cfg.template /etc/shinken/hosts/localhost.cfg
-cp /vagrant/vagrant_provision/livestatus.cfg.template /etc/shinken/modules/livestatus.cfg
+#cp /vagrant/vagrant_provision/broker-master.cfg.template /etc/shinken/brokers/broker-master.cfg
+#cp /vagrant/vagrant_provision/livestatus.cfg.template /etc/shinken/modules/livestatus.cfg
+rsync -avlp /vagrant/vagrant_provision/shinken/ /etc/shinken/
 
 # Launch the installer
 # We do not use the "install" target as it will  install the on-reader lib by copying the files
@@ -61,5 +74,7 @@ update-rc.d carbon defaults
 
 #
 # Start things up
-/etc/init.d/carbon start
+#/etc/init.d/carbon start
 service shinken restart
+service snmpd restart
+service openntpd restart
