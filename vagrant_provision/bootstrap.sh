72e1d22e9393d24e20d9bdb906dd65b9ff925969
Xavier Roger-Machart <xrm@mm.st>
2015-10-28 15:08:31 +0100

    Added the Nanto dependencies in the Vagrant bootstrap script

--- vagrant_provision/bootstrap.sh
+++ vagrant_provision/bootstrap.sh
@@ -3,9 +3,13 @@
 export DEBIAN_FRONTEND=noninteractive
 export PERL_MM_USE_DEFAULT=true
 
+# Add repositories for R
+sed -i '$ a\deb http://cran.univ-lyon1.fr/bin/linux/debian wheezy-cran3/' /etc/apt/sources.list
+apt-key adv --keyserver keys.gnupg.net --recv-key 381BA480
+
 aptitude -q update
 aptitude -y -q full-upgrade
-aptitude -y -q install vim git python-pip python-pycurl sqlite3 graphviz graphviz-dev pkg-config python-dev libxml2-dev
+aptitude -y -q install vim git python-pip python-pycurl sqlite3 graphviz graphviz-dev pkg-config python-dev libxml2-dev r-base-core liblzma-dev
 
 # Configure snmpd
 cp /vagrant/vagrant_provision/snmpd.conf.template /etc/snmp/snmpd.conf
@@ -37,13 +41,19 @@ cp /vagrant/vagrant_provision/livestatus.cfg.template /etc/shinken/modules/lives
 # Launch the installer
 # We do not use the "install" target because we we to create symlinks to the development files
 # instead of copies to facilitate development.
-make graphite shinken on-reader clean
+make graphite shinken on-reader nanto-libs clean
 
 # Create symbolic links for Hokuto
 ln -s /vagrant/hokuto/standalone /usr/local/hokuto
 ln -s /vagrant/hokuto/etc/hokuto.cfg /etc/hokuto.cfg
 ln -s /vagrant/hokuto/etc/init.d/hokuto /etc/init.d/hokuto
 update-rc.d hokuto defaults
+
+# Create symbolic links for Nanto
+ln -s /vagrant/nanto/src /usr/local/nanto
+cp /vagrant/nanto/etc/nanto.cfg /etc/nanto.cfg
+ln -s /vagrant/nanto/etc/init.d/nanto /etc/init.d/nanto
+update-rc.d nanto defaults
 
 # Auto start the carbon daemon on launch
 cp /vagrant/vagrant_provision/carbon.init.template /etc/init.d/carbon
@@ -66,3 +76,4 @@ update-rc.d carbon defaults
 /etc/init.d/carbon start
 service shinken restart
 service hokuto start
+service nanto start
