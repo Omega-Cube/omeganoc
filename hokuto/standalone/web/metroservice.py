b92407300ecb7e7a7e781c65fff2d00fa9a484f4
Xavier Roger-Machart <xrm@mm.st>
2016-08-16 11:31:17 +0200

    Added licence to metroservice

--- hokuto/standalone/web/metroservice.py
+++ hokuto/standalone/web/metroservice.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of Omega Noc
+# Copyright Omega Noc (C) 2016 Omega Cube and contributors
+# Xavier Roger-Machart, xrm@omegacube.fr
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """ Metrology data web services """
 
 import re
@@ -62,7 +82,6 @@ def _flatten_name(name):
 @app.route('/services/metrics')
 @login_required
 def get_new_metrics_list():
-    # TODO : filter unathorized elements
     result = {}
     client = _create_connection()
     tagdata = client.query('show tag values with key in ("host_name", "service_description")')
