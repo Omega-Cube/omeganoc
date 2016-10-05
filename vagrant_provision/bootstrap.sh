0e54656d8d47bc85cffe0e3362564da4bc1aac13
Xavier Roger-Machart <xrm@mm.st>
2016-10-05 16:01:51 +0200

    Some enhancements to the vagrant install script

--- vagrant_provision/bootstrap.sh
+++ vagrant_provision/bootstrap.sh
@@ -4,12 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 export PERL_MM_USE_DEFAULT=true
 
 # Add repositories for R
-sed -i '$ a\deb http://cran.univ-lyon1.fr/bin/linux/debian wheezy-cran3/' /etc/apt/sources.list
+sed -i '$ a\deb http://cran.univ-lyon1.fr/bin/linux/debian jessie-cran3/' /etc/apt/sources.list
 apt-key adv --keyserver keys.gnupg.net --recv-key 381BA480
 
 aptitude -q update
 aptitude -y -q full-upgrade
-aptitude -y -q install vim git python-pip python-pycurl sqlite3 graphviz graphviz-dev pkg-config python-dev libxml2-dev r-base-core liblzma-dev
+aptitude -y -q install vim git snmpd python-pip python-pycurl sqlite3 graphviz graphviz-dev pkg-config python-dev libxml2-dev r-base-core liblzma-dev
 
 # Configure snmpd
 cp /vagrant/vagrant_provision/snmpd.conf.template /etc/snmp/snmpd.conf
@@ -43,18 +43,21 @@ cp /vagrant/vagrant_provision/livestatus.cfg.template /etc/shinken/modules/lives
 make shinken on-reader nanto-libs clean
 
 # Create symbolic links for Hokuto
+#TODO : Remove files before replacing with the links
+rm -rf /usr/local/hokuto
 ln -s /vagrant/hokuto/standalone /usr/local/hokuto
+rm /etc/hokuto.cfg
 ln -s /vagrant/hokuto/etc/hokuto.cfg /etc/hokuto.cfg
-ln -s /vagrant/hokuto/etc/init.d/hokuto /etc/init.d/hokuto
-update-rc.d hokuto defaults
+#ln -s /vagrant/hokuto/etc/init.d/hokuto /etc/init.d/hokuto
+#update-rc.d hokuto defaults
 
 # Create symbolic links for Nanto
+rm -rf /usr/local/nanto
 ln -s /vagrant/nanto/src /usr/local/nanto
+rm /etc/nanto.cfg
 cp /vagrant/nanto/etc/nanto.cfg /etc/nanto.cfg
-ln -s /vagrant/nanto/etc/init.d/nanto /etc/init.d/nanto
-update-rc.d nanto defaults
-
-# with symbolic links hokuto and on_reader are not readable from shinken services which prevent shinken from loading hokuto.
+#ln -s /vagrant/nanto/etc/init.d/nanto /etc/init.d/nanto
+#update-rc.d nanto defaults
 
 # Make the lib folder available globally for the shinken and vagrant users
 #mkdir -p /home/shinken/.local/lib/python2.7/site-packages
